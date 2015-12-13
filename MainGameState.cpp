d1263351acb3e3e40a6a20250a898621c0f19052
lapinozz <monstrefou@gmail.com>
2015-12-13 14:25:27 -0500

    spawing even when choosing

--- MainGameState.cpp
+++ MainGameState.cpp
@@ -4,7 +4,8 @@
 
 #include <iostream>
 
-MainGameState::MainGameState(Game* t_game) : State(t_game), map({500, 500}, 600, t_game), generator(game) {
+MainGameState::MainGameState(Game* t_game) : State(t_game), map({500, 500}, 600, t_game), generator(game)
+{
 
 //	decisionShape1.setSize({ 512 - 96, 242.0f - 32 });
 //	decisionShape2.setSize({ 512 - 96, 242.0f - 32 });
@@ -14,101 +15,107 @@ MainGameState::MainGameState(Game* t_game) : State(t_game), map({500, 500}, 600,
     decisionShape1.setSize({(312.f/1024.f)*windowSize.x,(100.f/580.f)*windowSize.y});
     decisionShape2.setSize(decisionShape1.getSize());
 
-	decisionShape1.setPosition({ 20, windowSize.y - decisionShape1.getSize().y - 20});
-	decisionShape2.setPosition({ windowSize.x - decisionShape2.getSize().x - 20, windowSize.y - decisionShape1.getSize().y - 20});
+    decisionShape1.setPosition({ 20, windowSize.y - decisionShape1.getSize().y - 20});
+    decisionShape2.setPosition({ windowSize.x - decisionShape2.getSize().x - 20, windowSize.y - decisionShape1.getSize().y - 20});
 
-	decisionText1.setCharacterSize(12);
-	decisionText2.setCharacterSize(12);
+    decisionText1.setCharacterSize(12);
+    decisionText2.setCharacterSize(12);
 
-	decisionText1.setPosition(decisionShape1.getPosition() + decisionShape1.getSize()/2.f);
-	decisionText2.setPosition(decisionShape2.getPosition() + decisionShape2.getSize()/2.f);
+    decisionText1.setPosition(decisionShape1.getPosition() + decisionShape1.getSize()/2.f);
+    decisionText2.setPosition(decisionShape2.getPosition() + decisionShape2.getSize()/2.f);
 
-	sf::Font* font = &game->fonts.acquire("font", thor::ResourceLoader<sf::Font>(thor::Resources::fromFile<sf::Font>("assets/font.ttf")));
+    sf::Font* font = &game->fonts.acquire("font", thor::ResourceLoader<sf::Font>(thor::Resources::fromFile<sf::Font>("assets/font.ttf")));
 
-	decisionText1.setFont(*font);
-	decisionText2.setFont(*font);
+    decisionText1.setFont(*font);
+    decisionText2.setFont(*font);
 
-	killCountLeft.setCharacterSize(24);
-	killCountRight.setCharacterSize(killCountLeft.getCharacterSize());
+    killCountLeft.setCharacterSize(24);
+    killCountRight.setCharacterSize(killCountLeft.getCharacterSize());
 
-	killCountLeft.setFont(*font);
-	killCountRight.setFont(*font);
+    killCountLeft.setFont(*font);
+    killCountRight.setFont(*font);
 
-	killCountLeft.setPosition({20, 20});
-	killCountRight.setPosition({20, windowSize.x - 20 - getSize(killCountRight).x});
+    killCountLeft.setPosition({20, 20});
+    killCountRight.setPosition({20, windowSize.x - 20 - getSize(killCountRight).x});
 
-	frame.setPosition(0, 0);
-	background.setPosition(0, 0);
-	frame.setSize((sf::Vector2f)game->window.getSize());
-	background.setSize((sf::Vector2f)game->window.getSize());
-	frame.setTexture(&game->textures.acquire("frame", thor::Resources::fromFile<sf::Texture>("assets/frame.png")));
-	background.setTexture(&game->textures.acquire("background", thor::Resources::fromFile<sf::Texture>("assets/Background.png")));
+    frame.setPosition(0, 0);
+    background.setPosition(0, 0);
+    frame.setSize((sf::Vector2f)game->window.getSize());
+    background.setSize((sf::Vector2f)game->window.getSize());
+    frame.setTexture(&game->textures.acquire("frame", thor::Resources::fromFile<sf::Texture>("assets/frame.png")));
+    background.setTexture(&game->textures.acquire("background", thor::Resources::fromFile<sf::Texture>("assets/Background.png")));
 
-	game->textures.acquire("Infantry", thor::Resources::fromFile<sf::Texture>("assets/all-new.png"));
-	game->textures.acquire("bullet", thor::Resources::fromFile<sf::Texture>("assets/bullet.png"));
+    game->textures.acquire("Infantry", thor::Resources::fromFile<sf::Texture>("assets/all-new.png"));
+    game->textures.acquire("bullet", thor::Resources::fromFile<sf::Texture>("assets/bullet.png"));
 //	game->textures.acquire("Infantry", thor::Resources::fromFile<sf::Texture>("assets/animation.png"));
 //	game->textures.acquire("Die", thor::Resources::fromFile<sf::Texture>("assets/die.png"));
 
     left.side = Map::LEFT;
     right.side = Map::RIGHT;
 
-	map.spawnUnit(Unit(game, &left), Map::LEFT);
-	map.spawnUnit(Unit(game, &right), Map::RIGHT);
+    map.spawnUnit(Unit(game, &left), Map::LEFT);
+    map.spawnUnit(Unit(game, &right), Map::RIGHT);
 
 
 //	dangerLevel = 20;
 }
 
-void MainGameState::update() {
+void MainGameState::update()
+{
 
-	if (substate == RUNNING) {
-		decisionTimer += game->deltaTime;
+    if (substate == RUNNING)
+    {
+        decisionTimer += game->deltaTime;
 
-		left.accumulator += game->deltaTime;
-		while (left.accumulator >= 1.f / (Player::baseSpawnRate *left.spawnRateModifier)) {
-			left.accumulator -= 1.f / (Player::baseSpawnRate * left.spawnRateModifier);
-			map.spawnUnit(Unit(game, &left), Map::LEFT);
-		}
+        if (decisionTimer >= decisionDelay)
+        {
+            d1 = generator.getDecision(dangerLevel);
+            d2 = generator.getDecision(dangerLevel, d1);
 
-		right.accumulator += game->deltaTime;
-		while (right.accumulator >= 1.f / (Player::baseSpawnRate * right.spawnRateModifier)) {
-			right.accumulator -= 1.f / (Player::baseSpawnRate * right.spawnRateModifier);
-			map.spawnUnit(Unit(game, &right), Map::RIGHT);
-		}
+            decisionShape1.setTexture(d1->image);
+            decisionShape2.setTexture(d2->image);
 
-		if (decisionTimer >= decisionDelay) {
-			d1 = generator.getDecision(dangerLevel);
-			d2 = generator.getDecision(dangerLevel, d1);
+            decisionText1.setString(d1->message);
+            decisionText2.setString(d2->message);
 
-			decisionShape1.setTexture(d1->image);
-			decisionShape2.setTexture(d2->image);
+            setTextSize(decisionText1, decisionShape1.getSize(), {10,50});
+            setTextSize(decisionText2, decisionShape2.getSize(), {10,50});
 
-			decisionText1.setString(d1->message);
-			decisionText2.setString(d2->message);
+            substate = CHOOSING;
+            decisionTimer = 0;
+        }
 
-			setTextSize(decisionText1, decisionShape1.getSize(), {10,50});
-			setTextSize(decisionText2, decisionShape2.getSize(), {10,50});
+    }
 
-			substate = CHOOSING;
-			decisionTimer = 0;
-		}
-
-	}
-
-	else if (substate == CHOOSING) {
+    else if (substate == CHOOSING)
+    {
 
 
-	}
+    }
+
+    left.accumulator += game->deltaTime;
+    while (left.accumulator >= 1.f / (Player::baseSpawnRate *left.spawnRateModifier))
+    {
+        left.accumulator -= 1.f / (Player::baseSpawnRate * left.spawnRateModifier);
+        map.spawnUnit(Unit(game, &left), Map::LEFT);
+    }
+
+    right.accumulator += game->deltaTime;
+    while (right.accumulator >= 1.f / (Player::baseSpawnRate * right.spawnRateModifier))
+    {
+        right.accumulator -= 1.f / (Player::baseSpawnRate * right.spawnRateModifier);
+        map.spawnUnit(Unit(game, &right), Map::RIGHT);
+    }
 
     killCountLeft.setString(std::to_string(left.deadCount));
     killCountRight.setString(std::to_string(right.deadCount));
     killCountRight.setPosition({game->window.getSize().x - 20 - getSize(killCountRight).x, 20});
 
-	game->debug.log("Info", std::to_string(game->deltaTime));
-	map.update(game->deltaTime);
+    game->debug.log("Info", std::to_string(game->deltaTime));
+    map.update(game->deltaTime);
 
-	bool leftSmaller = map.leftUnits.size() < map.rightUnits.size()/2;
-	if(leftSmaller || map.rightUnits.size() < map.leftUnits.size()/2)
+    bool leftSmaller = map.leftUnits.size() < map.rightUnits.size()/2;
+    if(leftSmaller || map.rightUnits.size() < map.leftUnits.size()/2)
     {
         int smallerCount = (leftSmaller ? map.leftUnits.size() : map.rightUnits.size());
         int biggerCount = (leftSmaller ? map.rightUnits.size() : map.leftUnits.size());
@@ -119,53 +126,62 @@ void MainGameState::update() {
 //        smaller.spawnRateModifier += smallerCount/biggerCount;
 //        if(smallerCount == 0)
 //            smaller.spawnRateModifier += bigger.spawnRateModifier * 2;
+
         smaller.spawnRateModifier = 10.f;
         bigger.spawnRateModifier = 1;
     }
 }
 
-void MainGameState::render(sf::RenderTarget& target) {
+void MainGameState::render(sf::RenderTarget& target)
+{
     target.draw(background);
 
-	map.render(target);
+    map.render(target);
 
     target.draw(frame);
 
-	if (substate == CHOOSING) {
-		target.draw(decisionShape1);
-		target.draw(decisionShape2);
-		target.draw(decisionText1);
-		target.draw(decisionText2);
-	}
+    if (substate == CHOOSING)
+    {
+        target.draw(decisionShape1);
+        target.draw(decisionShape2);
+        target.draw(decisionText1);
+        target.draw(decisionText2);
+    }
 
     target.draw(killCountLeft);
     target.draw(killCountRight);
 }
 
-void MainGameState::handleEvent(const sf::Event& event) {
+void MainGameState::handleEvent(const sf::Event& event)
+{
 
 
-	if (substate == CHOOSING) {
-		if (event.type == sf::Event::KeyPressed) {
-			if (event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::A) {
-				handleDecision(d1);
-			}
-			else if (event.key.code == sf::Keyboard::Right || event.key.code == sf::Keyboard::D) {
-				handleDecision(d2);
-			}
-		}
-	}
+    if (substate == CHOOSING)
+    {
+        if (event.type == sf::Event::KeyPressed)
+        {
+            if (event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::A)
+            {
+                handleDecision(d1);
+            }
+            else if (event.key.code == sf::Keyboard::Right || event.key.code == sf::Keyboard::D)
+            {
+                handleDecision(d2);
+            }
+        }
+    }
 
 
 }
 
-void MainGameState::handleDecision(Decision* decision) {
+void MainGameState::handleDecision(Decision* decision)
+{
 
-	substate = RUNNING;
+    substate = RUNNING;
 
-	decision->callback(game, &left);
-	decision->callback(game, &right);
+    decision->callback(game, &left);
+    decision->callback(game, &right);
 
-	dangerLevel++;
-	generator.updateDangerLevel(dangerLevel);
+    dangerLevel++;
+    generator.updateDangerLevel(dangerLevel);
 }
