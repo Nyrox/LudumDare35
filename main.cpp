b831b96f83fd546822c2ce182ee0d1ad4a7628bc
Nyrox <mark.oliver@hotmail.de>
2016-02-21 03:06:25 +0100

    Ludum Dare Release Commit - Following commits are not eligable for submission in LD 35

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+/*
+	Version r1.0 - Ludum Dare Release
+	Developers: 
+		Nyrox - Mark-Oliver Junge, Germany
+		Lapinozz - Andre Polomat, Canada
+
+
+*/
+
 void Nyrox() {
 
 }
